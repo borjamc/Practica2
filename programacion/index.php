0569088a00e501ffb6fcf0e54235fab5d499cc74
Borja Molina Catalan <bomoca@floridauniversitaria.es>
2016-09-27 08:59:57 +0200

    first commit

---
+++ programacion/index.php
@@ -0,0 +1,15 @@
+
+<html>
+  <body>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      <?php
+      //fichero index.php
+//creado por Paco Gomez
+        echo "Borja" . "<br>";
+        echo "Molina" . "<br>";
+        print "Catalan" . "<br>";
+      ?>
+  </head>
+  </body>
+</html>
